394146847727a713260e5beedc29e427383e635b
iamzub.in <zchoudhary.10@gmail.com>
2024-09-28 03:23:59 +0530

    ha ha ts pt.2

--- app/api/detect-ingredients/route.ts
+++
@@ -1,8 +0,0 @@
-// export async function POST(req: Request) {
-// // fetch api call to detect ingredients from image
-// const response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto', {
-//     method: 'GET',
-//     // body: image,
-//   });
-//   return response.json();   
-// }
